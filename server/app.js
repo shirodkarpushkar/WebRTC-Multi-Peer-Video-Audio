f280cebc7c84c3ce634be2a8a95a086981fff02b
shirodkarpushkar <pushkars@winjit.com>
2020-05-12 19:05:12 +0530

    minor bug fixes

--- server/app.js
+++ server/app.js
@@ -1,25 +1,37 @@
-var express = require('express')
-  
-var app = module.exports = express.createServer();
+var express = require("express");
 
-var io = require('socket.io')(app);
+var app = (module.exports = express.createServer());
 
-io.on('connection', function(socket){
-	io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
+var io = require("socket.io")(app);
 
-	socket.on('signal', (toId, message) => {
-		io.to(toId).emit('signal', socket.id, message);
-  	});
+io.on("connection", function (socket) {
+  io.sockets.emit(
+    "user-joined",
+    socket.id,
+    io.engine.clientsCount,
+    Object.keys(io.sockets.clients().sockets)
+  );
+  console.log("user Connected");
 
-    socket.on("message", function(data){
-		io.sockets.emit("broadcast-message", socket.id, data);
-    })
+  socket.on("signal", (toId, message) => {
+    console.log("message", message);
+    io.to(toId).emit("signal", socket.id, message);
+  });
 
-	socket.on('disconnect', function() {
-		io.sockets.emit("user-left", socket.id);
-	})
+  socket.on("message", function (data) {
+    console.log("data", data);
+    io.sockets.emit("broadcast-message", socket.id, data);
+  });
+
+  socket.on("disconnect", function () {
+    io.sockets.emit("user-left", socket.id);
+  });
 });
 
-app.listen(3000, function(){
-	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3001, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    app.address().port,
+    app.settings.env
+  );
 });
